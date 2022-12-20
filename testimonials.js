c7d16c3f5b23eef90877713b441923c9910303fe
OmarDiego <omar79521audi@gmail.com>
2022-12-19 21:11:58 -0400

    prototype-portfolio-alternative

---
+++ testimonials.js
@@ -0,0 +1,30 @@
+const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit debitis nulla in quidem esse deserunt. Error, neque debitis est facere nesciunt, dolor quia eligendi quibusdam quo nemo at animi?",
+    avatar: "profile4",
+    name: "Marcelo",
+    title: "Tecnico",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit debitis nulla in quidem esse deserunt. Error, neque debitis est facere nesciunt, dolor quia eligendi quibusdam quo nemo at animi?",
+    avatar: "profile1",
+    name: "Ernest Achiever",
+    title: "Web Developer",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit debitis nulla in quidem esse deserunt. Error, neque debitis est facere nesciunt, dolor quia eligendi quibusdam quo nemo at animi?",
+    avatar: "profile5",
+    name: "Asamoah Gyan",
+    title: "Footerbailer",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit debitis nulla in quidem esse deserunt. Error, neque debitis est facere nesciunt, dolor quia eligendi quibusdam quo nemo at animi?",
+    avatar: "profile7",
+    name: "Beatrice Aku",
+    title: "Politician",
+  },
+];
